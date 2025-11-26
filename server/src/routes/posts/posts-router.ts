c7b8a17bb2a642d07983eaa8cd63507262e64fc5
Harris Ryder <ryderharris@icloud.com>
2025-11-26 00:29:31 +0000

    Add posts routes with fetching and error handling
    - Implemented a GET route to retrieve the latest 50 posts from the database.
    - Added a parameterized GET route to fetch a specific post by ID, including validation and error handling for non-existent posts.
    - Integrated Zod for request validation to ensure proper post ID format.

--- server/src/routes/posts/posts-router.ts
+++ server/src/routes/posts/posts-router.ts
@@ -1,9 +1,51 @@
 import { Hono } from "hono";
+import { authRequiredMiddleware } from "../../middlewares/auth-required-middleware.ts";
+import { db } from "../../db/index.ts";
+import { postsTable } from "../../db/schema.ts";
+import { desc } from "drizzle-orm";
+import { selectPostSchema } from "../../db/schema.types.ts";
+import z from "zod";
+import { zValidator } from "@hono/zod-validator";
+import { eq } from "drizzle-orm";
 
 export const postsRouter = new Hono();
 
 const route = postsRouter
   .basePath("/posts")
-  .get("/ping", (c) => c.text("pong"));
+  .get("/", async (c) => {
+    const posts = await db
+      .select()
+      .from(postsTable)
+      .orderBy(desc(postsTable.created_at))
+      .limit(50);
+    return c.json({ success: true, posts });
+  })
+  .get(
+    "/:postId",
+    zValidator("param", z.object({ postId: selectPostSchema.shape.id })),
+    async (c) => {
+      const postId = c.req.valid("param").postId;
+
+      const post = await db
+        .select()
+        .from(postsTable)
+        .where(eq(postsTable.id, postId));
+
+      if (!post) {
+        return c.json<{ success: false; error: string }>(
+          {
+            success: false,
+            error: "post not found",
+          },
+          404
+        );
+      }
+
+      return c.json<{ success: true; data: { post: typeof post } }>(
+        { success: true, data: { post } },
+        201
+      );
+    }
+  );
 
 export type PostsRoute = typeof route;
