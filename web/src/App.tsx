4f1e48d7ed7945d68abf9203588b000afbdf5f5f
Harris Ryder <ryderharris@icloud.com>
2025-12-28 15:10:56 +0000

    seetup landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,8 @@
 export default function App() {
   return (
-    <div>
-      <h1>hi</h1>
+    <div className="w-full h-svh flex flex-col justify-center items-center">
+      <img src="./icon.svg" className="w-12 h-12"></img>
+      <h1 style={{ fontFamily: 'Climate Crisis', fontSize: '36px', letterSpacing: '-0.03em', lineHeight: '1' }} className="mt-4 uppercase">CREMA</h1>
     </div>
   );
 }
