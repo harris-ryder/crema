efe02fd9d66ba64fc7f1611cdf04337c3fddc283
Harris Ryder <ryderharris@icloud.com>
2026-01-01 22:43:04 +0000

    fix tab layout

--- native/app/(tabs)/_layout.tsx
+++ native/app/(tabs)/_layout.tsx
@@ -3,12 +3,8 @@ import { useEffect } from "react";
 import { useAuth } from "@/contexts/auth-context";
 import { CustomTabBar } from "@/components/custom-tab-bar";
 
-import Ionicons from "@expo/vector-icons/Ionicons";
-import { useTheme } from "@/src/design";
-
 export default function TabLayout() {
   const { user, isLoading } = useAuth();
-  const theme = useTheme();
 
   const router = useRouter();
 
@@ -37,28 +33,12 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "Feed",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "cafe" : "cafe-outline"}
-              color={color}
-              size={28}
-              style={{ marginBottom: focused ? -3 : 0 }}
-            />
-          ),
         }}
       />
       <Tabs.Screen
         name="activity"
         options={{
           title: "Activity",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "heart" : "heart-outline"}
-              color={color}
-              size={28}
-              style={{ marginBottom: focused ? -3 : 0 }}
-            />
-          ),
         }}
       />
       <Tabs.Screen
@@ -66,14 +46,6 @@ export default function TabLayout() {
         options={{
           title: "Profile",
           headerShown: false,
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "person-circle" : "person-circle-outline"}
-              color={color}
-              size={28}
-              style={{ marginBottom: focused ? -3 : 0 }}
-            />
-          ),
         }}
       />
     </Tabs>
